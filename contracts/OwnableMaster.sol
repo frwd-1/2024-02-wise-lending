1a8eb82187f8d9a48fafd2865ab37696e00685d4
frwd-1 <frwdlabs@gmail.com>
2024-03-02 11:31:50 -0700

    Audits

--- contracts/OwnableMaster.sol
+++ contracts/OwnableMaster.sol
@@ -6,8 +6,22 @@ error NoValue();
 error NotMaster();
 error NotProposed();
 
-contract OwnableMaster {
+// @audit add natspecc, follow conventions
+// state variables
+// master
+// proposedMaster
+// zero address
 
+// promises / invariants
+// contract OwnableMaster manages the master of the contract
+// _onlyMaster --> if msg.sender == master, continue
+// _onlyProposed --> if msg.sender == proposedMaster, continue
+// constructor --> deployer of the contract sets the master, but the master cannot be the zero address
+// proposeOwner --> the current master can propose a new master, ie the proposedOwner becomes proposed Master
+// claimOwnership --> in order for the proposedMaster to become the master, the proposedMaster must call this function
+// renounceOwnership --> the current master can renounce ownership, ie the master becomes the zero address
+
+contract OwnableMaster {
     address public master;
     address public proposedMaster;
 
@@ -18,10 +32,7 @@ contract OwnableMaster {
         _;
     }
 
-    function _onlyMaster()
-        private
-        view
-    {
+    function _onlyMaster() private view {
         if (msg.sender == master) {
             return;
         }
@@ -34,10 +45,7 @@ contract OwnableMaster {
         _;
     }
 
-    function _onlyProposed()
-        private
-        view
-    {
+    function _onlyProposed() private view {
         if (msg.sender == proposedMaster) {
             return;
         }
@@ -50,13 +58,9 @@ contract OwnableMaster {
         address indexed proposedMaster
     );
 
-    event RenouncedOwnership(
-        address indexed previousMaster
-    );
+    event RenouncedOwnership(address indexed previousMaster);
 
-    constructor(
-        address _master
-    ) {
+    constructor(address _master) {
         if (_master == ZERO_ADDRESS) {
             revert NoValue();
         }
@@ -66,33 +70,24 @@ contract OwnableMaster {
     /**
      * @dev Allows to propose next master.
      * Must be claimed by proposer.
+     * // @audit *correction - must be claimed by the proposee (proposedMaster)
      */
-    function proposeOwner(
-        address _proposedOwner
-    )
-        external
-        onlyMaster
-    {
+    function proposeOwner(address _proposedOwner) external onlyMaster {
         if (_proposedOwner == ZERO_ADDRESS) {
             revert NoValue();
         }
 
         proposedMaster = _proposedOwner;
 
-        emit MasterProposed(
-            msg.sender,
-            _proposedOwner
-        );
+        emit MasterProposed(msg.sender, _proposedOwner);
     }
 
     /**
      * @dev Allows to claim master role.
      * Must be called by proposer.
+     * // @audit *correction - must be called by the proposedMaster
      */
-    function claimOwnership()
-        external
-        onlyProposed
-    {
+    function claimOwnership() external onlyProposed {
         master = proposedMaster;
     }
 
@@ -100,15 +95,10 @@ contract OwnableMaster {
      * @dev Removes master role.
      * No ability to be in control.
      */
-    function renounceOwnership()
-        external
-        onlyMaster
-    {
+    function renounceOwnership() external onlyMaster {
         master = ZERO_ADDRESS;
         proposedMaster = ZERO_ADDRESS;
 
-        emit RenouncedOwnership(
-            msg.sender
-        );
+        emit RenouncedOwnership(msg.sender);
     }
 }
